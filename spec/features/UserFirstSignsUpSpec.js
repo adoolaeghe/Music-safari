f6b2c2ffeb41cde710b545d7a141a8159146abf7
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-08-30 14:20:43 +0100

    add structure for feature test

--- spec/features/UserFirstSignsUpSpec.js
+++ spec/features/UserFirstSignsUpSpec.js
@@ -1,5 +1,17 @@
-describe("Login", function() {
-  it("authenticates you as a Spotify user", function() {
+var Browser = require("zombie");
+var url = "http://work.krasimirtsonev.com/git/blog-posts/TestingWithZombieJS/site/";
+var browser = new Browser();
 
+describe("Login", function() {
+  it("should visit the site and see the login form", function(next) {
+      browser.visit(url, function(err) {
+          expect(browser.success).toBe(true);
+          expect(browser.query("input[value='Login']")).toBeDefined();
+          next();
+      });
+  });
+  it("should authenticate the user with spotify ", function(next) {
+      browser.pressButton('input[value="Login"]', function() {
+      expect(browser.html("body")).toContain("-----");});
   });
 });
