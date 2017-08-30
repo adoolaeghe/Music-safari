9f0569608e43998c0d9c2d16490880c8f343514c
annalaise <annalaise.gibbons@gmail.com>
2017-08-30 17:02:27 +0100

    rendering individual track names from API results

--- src/Interface.js
+++ src/Interface.js
@@ -1,11 +1,22 @@
 $(document).on('keypress', '#trackname', function() {
   if(event.which === 13) { // check the key was <enter>
     var input = $(this);
-    var trackname = input.val();
+    var trackname = (input.val()).split(' ').join('&');
     var spotifyApiRequester = new SpotifyApiRequester();
     var searchResults = spotifyApiRequester.searchForTrack(trackname);
     console.log(searchResults);
-    $(".song-list").append(searchResults.tracks.items[0].name);
+    var displayTrackList = function (searchResults) {
+      var tracks = searchResults.tracks.items;
+      console.log(tracks);
+      tracks.forEach(function(track) {
+        var trackLi = document.createElement("div");
+        var trackName = document.createTextNode(track.name);
+        trackLi.appendChild(trackName);
+        console.log(trackName);
+        $(".song-list").append(trackLi);
+      });
+    };
+    displayTrackList(searchResults);
   }
 });
 
