bd10eeb356192731e3fb7d3148082c30a2218a7b
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-08-31 10:49:35 +0100

    test passing for user authentication

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ var app = express();
 app.use(express.static(__dirname));
 console.log('Listening on 8888');
 app.listen(8888);
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+exports.closeServer = function(){
+  server.close();
+};
