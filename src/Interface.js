7fc6c598ec345f786ecaf87e7af4a6e458d69b94
James Ransome <jamesransome@msn.com>
2017-09-01 15:23:00 +0100

    deleting comments/pointless code

--- src/Interface.js
+++ src/Interface.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
   $('#user-name').hide();
-  // var params = getHashParams();
-  // musicSafari = new MusicSafari(params.access_token);
-  // musicSafari.displayUserName();
 
   $(document).on('keypress', '#trackname', function() {
     if(event.which === 13) { // check the key was <enter>
@@ -11,9 +8,5 @@ $(document).ready(function(){
       var searchResults = spotifyApiRequester.searchForTrack(trackname);
       trackListView.displayTrackList(searchResults);
     }
-  });
-
-  $("#open-map").click(function() {
-    googleMapObject.findLocation();
   });
 });
