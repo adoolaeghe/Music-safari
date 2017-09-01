48cb951d0f9b326bfa673ef5b5f814aee49423ff
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-09-01 15:51:27 +0100

    final final commit

--- src/views/TrackListView.js
+++ src/views/TrackListView.js
@@ -9,7 +9,7 @@
 
       var outputHtml = "<div class='container'><ul class='list-group list-group-flush'>";
       tracks.forEach(function(track){
-        outputHtml += "<li class='list-group-item'><div class='row'><div class='col-4'><img class='float-left album-thumbnail' src=" + track.album.images[0].url + "></div>";
+        outputHtml += "<li class='list-group-item'><div class='row'><div class='col-4'><img class='img-fluid' src=" + track.album.images[0].url + "></div>";
         outputHtml += "<div class='col-4'><p>" + track.name + " by ";
         outputHtml += track.artists[0].name + "</p></div>";
         // outputHtml += "<input type=submit value='Add to map' onclick=musicSafari.createTrackPinObject('" + track.id + "')></input>";
